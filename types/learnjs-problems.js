d9fdac237558ad3763be6b07a18211318d6ceada
Alyona Gnevusheva <tchernovaalena@gmail.com>
2021-03-10 12:11:03 +0300

    solved two  types problems from learn.js

--- types/learnjs-problems.js
+++ types/learnjs-problems.js
@@ -1,6 +1,19 @@
 //Что выведет этот скрипт?
-
 let name = "Ilya";
 alert( `hello ${1}` ); // "hello 1"
 alert( `hello ${"name"}` ); // "hello name"
 alert( `hello ${name}` ); // "hello Ilya"
+
+//Создайте скрипт, который запрашивает ввод двух чисел (используйте prompt) и после показывает их сумму.
+let input = prompt("пожалуйста, введите два числа, разделённые пробелом, например: 2 2");
+input = input.split(" ")
+alert(+input[0] + +input[1]);
+
+/* Напишите функцию random(min, max), которая генерирует случайное число 
+с плавающей точкой от min до max (но не включая max). */
+function random (minNum, maxNum) {
+    if (minNum - maxNum > 0 || minNum === maxNum) {
+      return "неверный ввод";
+    }
+    return Math.random() * (maxNum - minNum) + minNum;
+}
