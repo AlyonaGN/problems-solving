aa9be71d3abad7cf0180d35e7d22177dac1f2e87
Alyona Gnevusheva <tchernovaalena@gmail.com>
2021-03-25 18:18:06 +0300

    решена задача на даты

--- objects/objects.js
+++ objects/objects.js
@@ -66,3 +66,28 @@ const sumSalaries = (salaries) => {
   }
   return res;
 }
+
+/*Напишите функцию getWeekDay(date), показывающую день недели 
+в коротком формате: «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «ВС».*/
+
+const getWeekDay = (date) => {
+  const day = date.getDay();
+  switch(day){
+    case 0: 
+      return 'ВС';
+    case 1: 
+      return 'ПН';
+    case 2: 
+      return 'ВТ';
+    case 3: 
+      return 'СР';
+    case 4: 
+      return 'ЧТ';   
+    case 5: 
+      return 'ПТ';
+    case 6: 
+      return 'СБ';
+    default:
+      return NaN;
+  }
+}
