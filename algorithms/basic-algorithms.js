f37df59116762a44e50e70b6b0c1b11ce580ec1e
Alyona Gnevusheva <tchernovaalena@gmail.com>
2021-03-29 21:18:54 +0300

    Решена задача на рекурсию и циклы

--- algorithms/basic-algorithms.js
+++ algorithms/basic-algorithms.js
@@ -1,23 +1,68 @@
 /*Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.*/
 //решение через рекурсию
 const sumTo = (num) => {
-    if (num === 1) {
-      return num;
-    }
-    else {
-      return num + sumTo(num - 1);
-    }
+  if (num === 1) {
+    return num;
+  }
+  else {
+    return num + sumTo(num - 1);
+  }
 }
 //решение через арифметическую прогрессию
 const sumTo = (num) => {
-    return ((1 + 100) / 2) * 100;
+  return ((1 + 100) / 2) * 100;
 }
 
 //решение с циклом 
 const sumTo = (num) => {
-    let res = num; 
-    for (let i = num - 1; i > 0; i--) {
-      res = res + i;
+  let res = num;
+  for (let i = num - 1; i > 0; i--) {
+    res = res + i;
+  }
+  return res;
+}
+
+/*Факториал натурального числа – это число, умноженное на "себя минус один", 
+затем на "себя минус два", и так далее до 1. 
+Факториал n обозначается как n!*/
+
+const factorial = (num) => {
+  if (num === 1) {
+    return num;
+  }
+  else {
+    return num * (factorial(num - 1));
+  }
+}
+
+/* Допустим, у нас есть односвязный список (как описано в главе Рекурсия и стек):
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
     }
-      return res;
+  }
+};
+Напишите функцию printList(list), которая выводит элементы списка по одному.
+Сделайте два варианта решения: используя цикл и через рекурсию.
+ */
+
+const printListRecursive = (list) => {
+  console.log(list.value);
+  if (list.next) {
+    return printList(list.next);
+  }
+}
+
+const printListCyclic = (list) => {
+  while (list) {
+    console.log(list.next);
+    list = list.next;
+  }
 }
