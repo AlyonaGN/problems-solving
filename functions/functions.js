12050cea22ea26fc8e6b4e60cc3a477f159bb2cd
Alyona Gnevusheva <tchernovaalena@gmail.com>
2021-05-05 18:42:59 +0300

    решена задача на свойства функций

--- functions/functions.js
+++ functions/functions.js
@@ -25,3 +25,22 @@ function Calculator() {
       this.methods[name] = func;
     };
 }
+
+/* Напишите функцию sum, которая бы работала следующим образом:
+sum(1)(2) == 3; // 1 + 2
+sum(1)(2)(3) == 6; // 1 + 2 + 3
+sum(5)(-1)(2) == 6
+sum(6)(-1)(-2)(-3) == 0
+sum(0)(1)(2)(3)(4)(5) == 15 */
+
+const sum = (num) => {
+  let counter = num;
+  function sumIt(nextNum) {
+    counter +=nextNum;
+    return sumIt;
+  }
+  sumIt.toString = function(){
+    return counter;
+  }
+  return sumIt;
+}
